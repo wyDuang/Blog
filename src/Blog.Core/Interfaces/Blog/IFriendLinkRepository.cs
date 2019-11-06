b472217e1ff1d78ba55149ec8480b90ae75386de
wyDuang <1014558384@qq.com>
2019-11-06 18:32:01 +0800

    未完待续

---
+++ src/Blog.Core/Interfaces/Blog/IFriendLinkRepository.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Core.Interfaces
+{
+    public interface IFriendLinkRepository : IRepositoryBase<FriendLink>
+    {
+
+    }
+}
